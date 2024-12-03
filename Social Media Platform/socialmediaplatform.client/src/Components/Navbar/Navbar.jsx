c96ccf3a71fd0685d36d714668c04660d7039729
Adidinamite <adrian.aparaschivei04@gmail.com>
2024-12-03 18:28:41 +0200

    initial frontend

---
+++ Social Media Platform/socialmediaplatform.client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+﻿import './Navbar.css'
+const Navbar = () => {
+    const stickyNav = () => {
+        let headerHeight = document.querySelector('header').offsetHeight;
+        let navbar = document.querySelector('nav');
+        let scrollValue = window.scrollY;
+
+        if(scrollValue > headerHeight) {
+            navbar.classList.add('sticky');
+        }
+        else {
+            navbar.classList.remove('sticky');
+        }
+    };
+    window.addEventListener('scroll', stickyNav);
+    return (
+        <header>
+            <nav>
+                <div className = "mainNav">
+                    <a href="/home" className={"logo"}>FridgeQuestApp</a>
+                    <ul className={"navbar"}>
+                        <li className={"menuItem"}>
+                            <a href="/home" className={"menuLink"}>Home</a>
+                        </li>
+                        <li className={"menuItem"}>
+                            <a href="/home" className={"menuLink"}>About</a>
+                        </li>
+                        <li className={"menuItem"}>
+                            <a href="/home" className={"menuLink"}>Recipes</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
