af997229cf05052c3bb8b4ec9735bc71c25e2f58
Alex <alex.tamer@yahoo.com>
2024-12-03 17:20:22 +0200

    database init

---
+++ Social Media Platform/SocialMediaPlatform.Server/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaPlatform.Server.Models;
+
+public class Post
+{
+    [Key] public int Id { get; set; }
+    public string ContentType { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string Content { get; set; } = string.Empty;
+    
+
+    // o sa aiba si user
+}
