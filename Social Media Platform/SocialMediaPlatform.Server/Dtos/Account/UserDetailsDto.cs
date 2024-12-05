b520cb3d2cc8c9974f415baa2c35f43d8bb9cc6d
Adidinamite <adrian.aparaschivei04@gmail.com>
2024-12-05 15:55:42 +0200

    getUser dto

---
+++ Social Media Platform/SocialMediaPlatform.Server/Dtos/Account/UserDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialMediaPlatform.Server.Dtos.Account;
+
+public class UserDetailsDto
+{
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Description { get; set; }
+}
