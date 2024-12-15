74e44ac5a0b7bfed34f67eec2b78ffe73f025dcd
Aparaschivei Adrian <adrian.aparaschivei04@gmail.com>
2024-12-16 01:01:59 +0200

    profiles can be private or public, modal for profile edit

--- Social Media Platform/SocialMediaPlatform.Server/Dtos/Account/UserDetailsDto.cs
+++ Social Media Platform/SocialMediaPlatform.Server/Dtos/Account/UserDetailsDto.cs
@@ -6,4 +6,6 @@ public class UserDetailsDto
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Description { get; set; }
+    
+    public bool IsPrivate { get; set; }
 }
